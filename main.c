2b4069aeb00db0fd52a40ff9b01975a95c9e1ef7
nabidul-islam <115966853+nabidul-islam@users.noreply.github.com>
2022-12-27 22:35:18 +0600

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "splash.h"
 #include "login.h"
 #include "menu.h"
 
 int main (void)
 {
+    system("clear");
+    splash();
+    sleep(5);
+    login();
     return 0;
 }
