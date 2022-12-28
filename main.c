4773a338c0337db05c5ec7f5c2ee36e2af899f20
nabidul-islam <115966853+nabidul-islam@users.noreply.github.com>
2022-12-28 17:57:00 +0600

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include "splash.h"
+#include "welcome.h"
 #include "login.h"
+#include "signup.h"
 #include "menu.h"
+
 
 int main (void)
 {
     system("clear");
+
     splash();
-    sleep(5);
+    sleep(2);
     system("clear");
-    login();
+
+    welcome();
+
+    printf("\n\n");
+    int e =0;
+    while (e <= 4)
+    {
+        printf("\t\t\t\t\t\tPress 1 to SIGN IN!\n");
+        printf("\t\t\t\t\t\tPress 2 to SIGN UP!\n");
+        printf("\t\t\t\t\t\tPress 3 to Exit!\n");
+
+        int check;
+        printf("\n\n\t\t\t\t\t\tEnter your Choose > ");
+        scanf("%d", &check);
+
+        if(check == 1){
+            login();
+        } else if(check == 2){
+            signup();
+        } else if(check == 3){
+            printf("\n\n\t\t\t\t\t\tExiting program!\n\n");
+            exit(EXIT_SUCCESS);
+        } else {
+            system("clear");
+            printf("\t\t\t\t\t\tworng input!\n\t\t\t\t\t\t\ttry again....\n\n");
+        }
+    }
     
+
+
+    menu();
+
     return 0;
 }
