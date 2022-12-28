f9e1c2ecc5b9bebc1abf0eb8d9bc6a6031db9a14
nabidul-islam <115966853+nabidul-islam@users.noreply.github.com>
2022-12-28 22:02:18 +0600

    Update login.h

--- login.h
+++ login.h
@@ -1,30 +1,56 @@
-login(){
+void login(void){
 
     char user_name_check[100], 
     pass_check[100];
 
     FILE *log;
     log = fopen("login.txt", "r");
-    
-    char check[100];
 
+    char check[100];
+    fscanf(log, "%s", check);
+
+    FILE *log_check;
+    log_check = fopen("check_user_pass.txt", "w");
+    
     printf("\t\t\t\t\t\t\t\tLOG IN\n");
     printf("\n\n");
 
     printf("\t\t\t\t\t\tUser Name [ ");
     scanf(" %s", &user_name_check);
+    fprintf(log_check ,"%s ", user_name_check);
+
     printf("\n");
 
     printf("\t\t\t\t\t\tPassword [ ");
     scanf(" %s", &pass_check);
+    fprintf(log_check ,"%s ", pass_check);
     printf("\n");
 
-    if(strcmp(user_name_check, user_name) && strcmp(pass_check, pass)){
-        printf("login successfull\n");
-    } else {
-        printf("wrong password or username!");
-    }
+    fclose(log_check);
 
-    fclose(log);
+    log_check = fopen("check_user_pass.txt", "r");
+
+    char temp[100]; 
+    fscanf(log_check, "%s", temp);
+
+    while (fopen("login.txt", "r"))
+    {
+        if(strcmp(temp,check) == 0){
+            printf("Login successfull\n");
+            system("clear");
+            menu();
+            break;
+
+        } else {
+            system("clear");
+            printf("\t\t\t\t\t\tWrong Password or Username! \n\t\t\t\t\t\t\tPlease try again...\n\n");
+            login();
+            break;
+        }
+        
+    }
     
+
+    fclose(log_check);
+    fclose(log);
 }
