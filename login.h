52b51924146af819109e6f3cf25a875eeca350d6
nabidul-islam <115966853+nabidul-islam@users.noreply.github.com>
2022-12-28 18:23:36 +0600

    Update login.h

--- login.h
+++ login.h
@@ -1,4 +1,4 @@
-void login(char user_name[100], char pass[100]){
+login(){
 
     char user_name_check[100], 
     pass_check[100];
@@ -6,19 +6,25 @@ void login(char user_name[100], char pass[100]){
     FILE *log;
     log = fopen("login.txt", "r");
     
-
+    char check[100];
 
     printf("\t\t\t\t\t\t\t\tLOG IN\n");
     printf("\n\n");
 
     printf("\t\t\t\t\t\tUser Name [ ");
-    scanf("%s", &user_name_check);
+    scanf(" %s", &user_name_check);
     printf("\n");
 
     printf("\t\t\t\t\t\tPassword [ ");
-    scanf("%s", &pass_check);
-    printf("\n");  
+    scanf(" %s", &pass_check);
+    printf("\n");
 
-    
+    if(strcmp(user_name_check, user_name) && strcmp(pass_check, pass)){
+        printf("login successfull\n");
+    } else {
+        printf("wrong password or username!");
+    }
+
+    fclose(log);
     
 }
