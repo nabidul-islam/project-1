a0129b4e2012dde2469265adf9af1e958393f1c1
nabidul-islam <115966853+nabidul-islam@users.noreply.github.com>
2022-12-29 13:45:30 +0600

    Update submenu.h

--- submenu.h
+++ submenu.h
@@ -1,3 +1,27 @@
-void submenu(void){
-    printf("Select Date\n\n");
+void submenu(void) {
+    int date;
+    printf("Select Date ");
+
+    time_t t = time(NULL);
+    struct tm time = * localtime( & t);
+    //   printf("date - %d\n", time.tm_mday);  
+
+    for (int i = time.tm_mday; i <= time.tm_mday; i++) {
+        if (i <= 31) {
+            printf("\t%d ", time.tm_mday++);
+        } else {
+            break;
+        }
+    }
+    printf("Enter Date : ");
+    scanf("%d", &date);
+
+    printf("Select Movie(5)");
+    printf("\tAvatar: The Way of Water (3D)");
+    printf("\tAvatar: The Way of Water (2D)");
+    printf("\tBlack Panther Wakanda Forever (2D)");
+    printf("\tDamal(2D)");
+    printf("\tBlack Adam");
+
+
 }
