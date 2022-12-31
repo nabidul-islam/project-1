565e2117f06f4869f29ec80be2cbc79863f3119d
nabidul-islam <115966853+nabidul-islam@users.noreply.github.com>
2022-12-31 23:10:36 +0600

    Update welcome.h

--- welcome.h
+++ welcome.h
@@ -1,9 +1,9 @@
 void welcome(void){
 
     int x, y;
-    char text1[] = "\n\n\t\t\t\t\t\t\t Welcome ";
-    char text2[] = "\n\t\t\t\t\t\t\t    To ";
-    char text3[] = "\n\t\t\t\t\t\tMovie_Ticket_Booking_System ";
+    char text1[] = "\n\n\t\t\t\t\t\t\t\t\t Welcome ";
+    char text2[] = "\n\t\t\t\t\t\t\t\t\t    To ";
+    char text3[] = "\n\t\t\t\t\t\t\t\tMovie_Ticket_Booking_System ";
 
     printf("\n\n");
     for (x = 0; text1[x] != NULL; x++) {
@@ -23,4 +23,5 @@ void welcome(void){
         for (y = 0; y <= 9999999; y++){}
     }
     printf("\n\n");
+    sleep(1);
 }
