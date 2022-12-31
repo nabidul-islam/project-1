9ec350ca8e9faf90c39c4fe668dc4e71ce5e49fd
nabidul-islam <115966853+nabidul-islam@users.noreply.github.com>
2022-12-31 23:10:40 +0600

    Update signup.h

--- signup.h
+++ signup.h
@@ -5,19 +5,19 @@ void signup(void) {
     struct user_data l;
 
     
-    printf("\t\t\t\t\t\t\t\tSIGN UP\n");
+    printf("\t\t\t\t\t\t\t\t\tSIGN UP\n\n");
 
-    printf("\t\t\t\t\t\tFirst Name : ");
+    printf("\t\t\t\t\t\t\tFirst Name : ");
     scanf("%s", &l.first_name);
 
-    printf("\t\t\t\t\t\tLast Name : ");
+    printf("\t\t\t\t\t\t\tLast Name : ");
     scanf("%s", &l.last_name);
 
-    printf("\t\t\t\t\t\tUser Name : ");
+    printf("\t\t\t\t\t\t\tUser Name : ");
     scanf("%s", &l.user_name);
     fprintf(log, "%s", l.user_name);
 
-    printf("\t\t\t\t\t\tPassword : ");
+    printf("\t\t\t\t\t\t\tPassword : ");
     scanf("%s", &l.pass);
     fprintf(log, "%s", l.pass);
     
