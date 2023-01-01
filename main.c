1c6fa36ed6d697849b76f71e161d86a0deb4451b
nabidul-islam <115966853+nabidul-islam@users.noreply.github.com>
2023-01-01 12:58:47 +0600

    Update main.c

--- main.c
+++ main.c
@@ -38,45 +38,45 @@ int main (void)
 {
     system("clear");
 
-    // splash();
-    // sleep(2);
-    // system("clear");
+    splash();
+    sleep(2);
+    system("clear");
 
-    // welcome();
+    welcome();
 
-    // printf("\n\n");
-    // int e = 0;
-    // while (e <= 4)
-    // {
-    //     printf("\t\t\t\t\t\tPress 1 to SIGN IN!\n");
-    //     printf("\t\t\t\t\t\tPress 2 to SIGN UP!\n");
-    //     printf("\t\t\t\t\t\tPress 3 to Exit!\n");
+    printf("\n\n");
+    int e = 0;
+    while (e <= 4)
+    {
+        printf("\t\t\t\t\t\tPress 1 to SIGN IN!\n");
+        printf("\t\t\t\t\t\tPress 2 to SIGN UP!\n");
+        printf("\t\t\t\t\t\tPress 3 to Exit!\n");
 
-    //     int check;
-    //     printf("\n\n\t\t\t\t\t\tEnter your Choose > ");
-    //     scanf("%d", &check);
+        int check;
+        printf("\n\n\t\t\t\t\t\tEnter your Choose > ");
+        scanf("%d", &check);
 
-    //     if(check == 1){
-    //         system("clear");
-    //         login();
-    //         break;
-    //     } else if(check == 2){
-    //         system("clear");
-    //         signup();
-    //         break;
-    //     } else if(check == 3){
-    //         printf("\n\n\t\t\t\t\t\tExiting program!\n\n");
-    //         exit(EXIT_SUCCESS);
-    //     } else {
-    //         system("clear");
-    //         printf("\t\t\t\t\t\tWorng input!\n\t\t\t\t\t\t\ttry again....\n\n");
-    //     }
-    // }
+        if(check == 1){
+            system("clear");
+            login();
+            break;
+        } else if(check == 2){
+            system("clear");
+            signup();
+            break;
+        } else if(check == 3){
+            printf("\n\n\t\t\t\t\t\tExiting program!\n\n");
+            exit(EXIT_SUCCESS);
+        } else {
+            system("clear");
+            printf("\t\t\t\t\t\tWorng input!\n\t\t\t\t\t\t\ttry again....\n\n");
+        }
+    }
 
     
-    // system("clear");
+    system("clear");
     
-    // printf("\n");
+    printf("\n");
     menu();
 
     return 0;
